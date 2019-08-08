295458e970213602bc2f972a852286e7a8c5795a
Andreas Kleineberg <buddy@akdigitalventures.de>
2019-08-08 09:40:27 +0200

    refactoring

--- django_minio/storage.py
+++ django_minio/storage.py
@@ -6,7 +6,8 @@ from django.conf import settings
 from django.core.files.storage import Storage
 from django.utils.deconstruct import deconstructible
 from minio import Minio
-from minio.error import InvalidXMLError, InvalidEndpointError, NoSuchKey, NoSuchBucket
+from minio.error import (InvalidXMLError, InvalidEndpointError, NoSuchKey,
+                         NoSuchBucket)
 from urllib3.exceptions import MaxRetryError
 
 
@@ -20,15 +21,12 @@ def setting(name, default=None):
 
 @deconstructible
 class MinioStorage(Storage):
-    # TODO: Log errors caused by exceptions
-    server = setting('MINIO_SERVER')
-    access_key = setting('MINIO_ACCESSKEY')
-    secret_key = setting('MINIO_SECRET')
-    bucket = setting('MINIO_BUCKET')
-    secure = setting('MINIO_SECURE')
-
     def __init__(self, *args, **kwargs):
-        super(MinioStorage, self).__init__(*args, **kwargs)
+        self.SERVER = setting('MINIO_SERVER')
+        self.ACCESS_KEY = setting('MINIO_ACCESSKEY')
+        self.SECRET_KEY = setting('MINIO_SECRET')
+        self.BUCKET = setting('MINIO_BUCKET')
+        self.SECURE = setting('MINIO_SECURE')
         self._connection = None
 
     @property
@@ -36,29 +34,31 @@ class MinioStorage(Storage):
         if not self._connection:
             try:
                 self._connection = Minio(self.server,
-                                         access_key=self.access_key,
-                                         secret_key=self.secret_key,
-                                         secure=self.secure)
+                                         access_key=self.ACCESS_KEY,
+                                         secret_key=self.SECRET_KEY,
+                                         secure=self.SECURE)
             except InvalidEndpointError:
                 self._connection = None
         return self._connection
 
     def _open(self, object_name, mode):
-        return self.connection.get_object(self.bucket, object_name)
+        return self.connection.get_object(self.BUCKET, object_name)
 
     def _save(self, name, content):
         pathname, ext = os.path.splitext(name)
         dir_path, _ = os.path.split(pathname)
         hashed_name = f'{dir_path}/{hash(content)}{ext}'
 
-        content_type = content.content_type if hasattr(content, 'content_type') else mimetypes.guess_type(name)[0]
+        content_type = (content.content_type
+                        if hasattr(content, 'content_type')
+                        else mimetypes.guess_type(name)[0])
 
         if self.connection:
-            if not self.connection.bucket_exists(self.bucket):
-                self.connection.make_bucket(self.bucket)
+            if not self.connection.bucket_exists(self.BUCKET):
+                self.connection.make_bucket(self.BUCKET)
             try:
                 self.connection.put_object(
-                    self.bucket,
+                    self.BUCKET,
                     hashed_name,
                     content,
                     content.size,
@@ -72,15 +72,15 @@ class MinioStorage(Storage):
 
     def delete(self, name):
         object_name = Path(name).as_posix()
-        self.connection.remove_object(bucket_name=self.bucket,
+        self.connection.remove_object(bucket_name=self.BUCKET,
                                       object_name=object_name)
 
     def url(self, name):
-        return f'{self.server}/{self.bucket}/{name}'
+        return f'{self.SERVER}/{self.BUCKET}/{name}'
 
     def exists(self, name):
         try:
-            self.connection.stat_object(self.bucket, name)
+            self.connection.stat_object(self.BUCKET, name)
         except (NoSuchKey, NoSuchBucket):
             return False
         except Exception as err:
@@ -89,4 +89,4 @@ class MinioStorage(Storage):
             return True
 
     def size(self, name):
-        return self.connection.stat_object(self.bucket, name).size
+        return self.connection.stat_object(self.BUCKET, name).size
